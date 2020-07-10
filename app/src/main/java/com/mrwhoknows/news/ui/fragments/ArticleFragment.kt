d686ae263231c7b9fc233d8113a01f5bf10e8052
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-07-10 15:20:15 +0530

    added navigation with nav graph

---
+++ app/src/main/java/com/mrwhoknows/news/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,18 @@
+package com.mrwhoknows.news.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mrwhoknows.news.R
+
+class ArticleFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_article, container, false)
+    }
+}
