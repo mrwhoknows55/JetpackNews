6b3146d158bb59fdef740c0cecfa8553c37c9efc
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-07-10 17:08:53 +0530

    added retrofit instance

--- app/src/main/java/com/mrwhoknows/news/api/NewsAPI.kt
+++ app/src/main/java/com/mrwhoknows/news/api/NewsAPI.kt
@@ -1,6 +1,8 @@
 package com.mrwhoknows.news.api
 
-import com.mrwhoknows.news.util.Keys.Companion.API_KEY
+import com.mrwhoknows.news.model.NewsResponse
+import com.mrwhoknows.news.util.Constants.Companion.API_KEY
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -13,6 +15,15 @@ interface NewsAPI {
         pageNo: Int = 1,
         @Query("apiKey")
         apiKey: String = API_KEY
-    )
+    ): Response<NewsResponse>
 
+    @GET("v2/everything")
+    suspend fun serachForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNo: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
 }
