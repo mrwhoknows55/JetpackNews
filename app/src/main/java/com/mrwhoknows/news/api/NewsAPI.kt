66da5a207255aa36f90e47d9524f532f98721ba3
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-07-10 16:50:51 +0530

    added model classes

---
+++ app/src/main/java/com/mrwhoknows/news/api/NewsAPI.kt
@@ -0,0 +1,18 @@
+package com.mrwhoknows.news.api
+
+import com.mrwhoknows.news.util.Keys.Companion.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNo: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    )
+
+}
