0bf74ce6cfa9db895ff1ed29cc4f1746d5edbd4a
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-07-11 23:14:55 +0530

    fixed retrofit instantiation

--- app/src/main/java/com/mrwhoknows/news/repository/NewsRepository.kt
+++ app/src/main/java/com/mrwhoknows/news/repository/NewsRepository.kt
@@ -1,8 +1,13 @@
 package com.mrwhoknows.news.repository
 
+import com.mrwhoknows.news.api.RetrofitInstance
 import com.mrwhoknows.news.db.ArticleDatabase
 
 class NewsRepository(
     val db: ArticleDatabase
 ) {
+
+    suspend fun getBreakingNews(countryCode: String, pageNo: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNo)
+
 }
